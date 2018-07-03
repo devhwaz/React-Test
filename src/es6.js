c62d4ce049abf640c93142e35604162cbe2da4b3
react화진 <devhwaz@gmail.com>
2018-07-03 15:56:39 +0900

    React Basic - Component 실습

---
+++ src/es6.js
@@ -0,0 +1,15 @@
+function f(){
+  return 1;
+}
+
+f();
+
+const f = () => {
+  return 1;
+};
+
+const f = () => 1
+
+const f = () => <div>xxxxxx</div>;
+
+const f = () => {return <div>xxxxxx</div>;}
